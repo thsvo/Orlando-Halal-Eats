7ea731a9f5da3c686f3daead4ddfa7c66d792702
thsvo <155262940+thsvo@users.noreply.github.com>
2025-04-04 10:43:56 +0600

    food fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -178,7 +178,7 @@ export default function Example() {
                   </DisclosurePanel>
                 </Disclosure> */}
                 <a
-                  href="/Foods"
+                  href="/foods"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
 
